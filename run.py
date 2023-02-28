84c90509a4bc59898aba127e6e7ee9f13eeb281f
Jens Lindgren <jens_lindgren93@hotmail.com>
2023-02-28 10:37:59 +0000

    Added validation for the input name, age, sex. And you will get a error message if you enter a invalid input.

--- run.py
+++ run.py
@@ -29,8 +29,12 @@ print()
 def input_name():
     '''
     Collect the user name. And return it.
+    Also check if the name is valid.
     '''
     name = input('Please enter your name: ')
+    while len(name) == 0:
+        print("invalid name")
+        name = input('Please enter your name:')
     print('Hello ' + name + '!')
     return name
 
@@ -40,8 +44,17 @@ def input_name():
 def input_age():
     '''
     collect the user age. And return it.
+    Also check if the age is valid.
     '''
     age = input('Please enter your age: ')
+    try:
+        age = int(age)
+        if age < 0 or age > 120:
+            print('Please enter a valid age!')
+            input_age()
+    except ValueError:
+        print('Please enter a valid age!')
+        input_age()
     print('You are ' + age + ' years old!')
     return age
 
@@ -51,8 +64,12 @@ def input_age():
 def input_sex():
     '''
     Collect the user sex. And return it.
+    Also check if the sex is valid.
     '''
     sex = input('Please enter your sex: ')
+    while sex not in ["male", "female"]:
+        print("Invalid sex")
+        sex = input('Please enter your sex: ')
     print('You are ' + sex + '!')
 
     return sex
@@ -65,6 +82,14 @@ def input_height():
     Collect the user height. And return it.
     '''
     height = input('Please enter your height: ')
+    try:
+        height = int(height)
+        if height < 130 or height > 230:
+            print('Please enter a valid height!')
+            input_height()
+    except ValueError:
+        print('Please enter a valid height!')
+        input_height()
     print('You are ' + height + ' cm!')
     return height
 
@@ -76,6 +101,14 @@ def input_weight():
     Collect the user weight. And return it.
     '''
     weight = input('Please enter your weight: ')
+    try:
+        weight = int(weight)
+        if weight < 40 or weight > 200:
+            print('Please enter a valid weight')
+            input_weight()
+    except ValueError:
+        print('Please enter a valid weight')
+        input_weight()
     print('You are ' + weight + ' kg!')
     return weight
 
